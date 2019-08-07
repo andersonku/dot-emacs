c051e853aea40562b4737e525a3841891b8b6e1c
Anderson Ku <andersonku@gmail.com>
2019-08-07 15:39:30 +1000

    Changes

--- init.el
+++ init.el
@@ -146,13 +146,6 @@ point reaches the beginning or end of the buffer, stop there."
 ;;; Packages
 ;;----------------------------------------------------------------------------
 
-(use-package js2-mode
-  :mode "\\.js\\'"
-  :bind (:map js2-mode-map ("C-c C-c" . compile)))
-
-(use-package dockerfile-mode
-  :mode ("Dockerfile\\'" . dockerfile-mode))
-
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
@@ -285,7 +278,6 @@ point reaches the beginning or end of the buffer, stop there."
 
 (use-package avy)
 
-
 (use-package avy-zap
   :bind
   (("M-z" . avy-zap-up-to-char-dwim)
@@ -346,6 +338,231 @@ point reaches the beginning or end of the buffer, stop there."
 :if (eq system-type 'darwin)
 :hook (dashboard-mode . (turn-on-pbcopy)))
 
+(use-package posframe)
+
+(use-package anzu
+  :diminish
+  :hook
+  (after-init . global-anzu-mode))
+
+(use-package flyspell
+  :diminish
+  :if (executable-find "aspell")
+  :hook
+  ((org-mode yaml-mode markdown-mode git-commit-mode) . flyspell-mode)
+  (prog-mode . flyspell-prog-mode)
+  (before-save-hook . flyspell-buffer)
+  (flyspell-mode . (lambda ()
+                     (dolist (key '("C-;" "C-," "C-."))
+                       (unbind-key key flyspell-mode-map))))
+  :custom
+  (flyspell-issue-message-flag nil)
+  (ispell-program-name "aspell")
+  (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
+  :custom-face
+  (flyspell-incorrect ((t (:underline (:color "#f1fa8c" :style wave)))))
+  (flyspell-duplicate ((t (:underline (:color "#50fa7b" :style wave)))))
+  :preface
+  (defun message-off-advice (oldfun &rest args)
+    "Quiet down messages in adviced OLDFUN."
+    (let ((message-off (make-symbol "message-off")))
+      (unwind-protect
+          (progn
+            (advice-add #'message :around #'ignore (list 'name message-off))
+            (apply oldfun args))
+        (advice-remove #'message message-off))))
+  :config
+  (advice-add #'ispell-init-process :around #'message-off-advice)
+  (use-package flyspell-correct-ivy
+    :bind ("C-M-:" . flyspell-correct-at-point)
+    :config
+    (when (eq system-type 'darwin)
+      (progn
+        (global-set-key (kbd "C-M-;") 'flyspell-correct-at-point)))
+    (setq flyspell-correct-interface #'flyspell-correct-ivy)))
+
+(use-package git-gutter
+    :custom
+    (git-gutter:modified-sign "~")		; 
+    (git-gutter:added-sign    "+")		; 
+    (git-gutter:deleted-sign  "-")		; 
+    :custom-face
+    (git-gutter:modified ((t (:foreground "#f1fa8c" :background "#f1fa8c"))))
+    (git-gutter:added    ((t (:foreground "#50fa7b" :background "#50fa7b"))))
+    (git-gutter:deleted  ((t (:foreground "#ff79c6" :background "#ff79c6"))))
+    :config
+    (global-git-gutter-mode +1))(use-package git-gutter
+    :custom
+    (git-gutter:modified-sign "~")		; 
+    (git-gutter:added-sign    "+")		; 
+    (git-gutter:deleted-sign  "-")		; 
+    :custom-face
+    (git-gutter:modified ((t (:foreground "#f1fa8c" :background "#f1fa8c"))))
+    (git-gutter:added    ((t (:foreground "#50fa7b" :background "#50fa7b"))))
+    (git-gutter:deleted  ((t (:foreground "#ff79c6" :background "#ff79c6"))))
+    :config
+    (global-git-gutter-mode +1))
+
+(use-package dashboard
+  :disabled
+  :diminish
+  (dashboard-mode page-break-lines-mode)
+  :custom
+  (dashboard-center-content t)
+  (dashboard-startup-banner 4)
+  (dashboard-items '((recents . 15)
+                     (projects . 5)
+                     (bookmarks . 5)))
+  :custom-face
+  (dashboard-heading ((t (:foreground "#f1fa8c" :weight bold))))
+  :hook
+  (after-init . dashboard-setup-startup-hook))
+
+(use-package display-line-numbers
+  :ensure nil
+  :hook
+  ((prog-mode yaml-mode systemd-mode) . display-line-numbers-mode))
+
+(use-package dimmer
+  :custom
+  (dimmer-fraction 0.5)
+  (dimmer-exclusion-regexp-list
+       '(".*Minibuf.*"
+         ".*which-key.*"
+         ".*NeoTree.*"
+         ".*Messages.*"
+         ".*Async.*"
+         ".*Warnings.*"
+         ".*LV.*"
+         ".*Ilist.*"))
+  :config
+  (dimmer-mode t))
+
+(use-package fill-column-indicator
+  :hook
+  ((markdown-mode
+    git-commit-mode) . fci-mode))
+
+(use-package hl-line
+  :ensure nil
+  :hook
+  (after-init . global-hl-line-mode))
+
+(use-package paren
+  :ensure nil
+  :hook
+  (after-init . show-paren-mode)
+  :custom-face
+  (show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c")))) ;; :box t
+  :custom
+  (show-paren-style 'mixed)
+  (show-paren-when-point-inside-paren t)
+  (show-paren-when-point-in-periphery t))
+
+(use-package highlight-symbol
+  :bind
+  (:map prog-mode-map
+  ("C-<f3>" . highlight-symbol)))
+
+(use-package beacon
+  ;; :custom
+  ;; (beacon-color "#f1fa8c")
+  :hook (after-init . beacon-mode))
+
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+(use-package bm
+  :bind (("C-c b b" . bm-toggle)
+         ("C-c b n" . bm-next)
+         ("C-c b p" . bm-previous))
+  :commands (bm-repository-load
+             bm-buffer-save
+             bm-buffer-save-all
+             bm-buffer-restore)
+  :init
+  (add-hook 'after-init-hook 'bm-repository-load)
+  (add-hook 'find-file-hooks 'bm-buffer-restore)
+  (add-hook 'after-revert-hook #'bm-buffer-restore)
+  (add-hook 'kill-buffer-hook #'bm-buffer-save)
+  (add-hook 'after-save-hook #'bm-buffer-save)
+  (add-hook 'vc-before-checkin-hook #'bm-buffer-save)
+  (add-hook 'kill-emacs-hook #'(lambda nil
+                                 (bm-buffer-save-all)
+                                 (bm-repository-save))))
+
+(use-package deadgrep
+  :bind ("C-'" . deadgrep))
+
+(use-package docker
+  :bind ("C-c d" . docker)
+  :diminish
+  :init
+  ;; (use-package docker-image     :commands docker-images)
+  ;; (use-package docker-container :commands docker-containers)
+  ;; (use-package docker-volume    :commands docker-volumes)
+  ;; (use-package docker-network   :commands docker-containers)
+  ;; (use-package docker-machine   :commands docker-machines)
+  )
+
+(use-package docker-compose-mode
+  :mode "docker-compose.*\.yml\\'")
+
+(use-package free-keys
+  :commands free-keys)
+
+(use-package goto-last-change
+  :bind ("C-x C-/" . goto-last-change))
+
+(use-package fzf)
+
+(use-package projectile
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+;;----------------------------------------------------------------------------
+;;; Languages
+;;----------------------------------------------------------------------------
+
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :bind (:map js2-mode-map ("C-c C-c" . compile)))
+
+(use-package json-mode
+  :mode "\\.json\\'")
+
+(use-package dockerfile-mode
+  :mode ("Dockerfile\\'" . dockerfile-mode))
+
+(use-package markdown-mode
+  :custom
+  (markdown-hide-markup nil)
+  (markdown-bold-underscore t)
+  (markdown-italic-underscore t)
+  (markdown-header-scaling t)
+  (markdown-indent-function t)
+  (markdown-enable-math t)
+  (markdown-hide-urls nil)
+  :custom-face
+  (markdown-header-delimiter-face ((t (:foreground "mediumpurple"))))
+  (markdown-header-face-1 ((t (:foreground "violet" :weight bold :height 1.0))))
+  (markdown-header-face-2 ((t (:foreground "lightslateblue" :weight bold :height 1.0))))
+  (markdown-header-face-3 ((t (:foreground "mediumpurple1" :weight bold :height 1.0))))
+  (markdown-link-face ((t (:background "#0e1014" :foreground "#bd93f9"))))
+  (markdown-list-face ((t (:foreground "mediumpurple"))))
+  (markdown-pre-face ((t (:foreground "#bd98fe"))))
+  :mode "\\.md\\'")
+
+(use-package markdown-toc)
+
+(use-package logview :defer t)
+
+(use-package yaml-mode
+  :mode ("\\.yaml\\'" "\\.yml\\'")
+  :custom-face
+  (font-lock-variable-name-face ((t (:foreground "violet")))))
+
 ;;----------------------------------------------------------------------------
 ;;; Theme
 ;;----------------------------------------------------------------------------
@@ -353,6 +570,10 @@ point reaches the beginning or end of the buffer, stop there."
   :config
   (load-theme 'zenburn t))
 
+;;; Guide Key-bindings https://github.com/justbur/emacs-which-key
+(use-package which-key
+  :diminish which-key-mode
+  :hook (after-init . which-key-mode))
 
 ;;----------------------------------------------------------------------------
 ;;; From my old config file
@@ -420,35 +641,37 @@ When `universal-argument' is called first, cut whole buffer (but respect `narrow
 
 (global-set-key (kbd "<f1>") 'help-command)
 (global-set-key (kbd "<f2>") 'xah-cut-line-or-region)  ; cut
+(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
+(global-set-key (kbd "<f4>") 'yank)
 (global-set-key (kbd "<C-f2>") 'bm-toggle)
 (global-set-key (kbd "<f10>")   'bm-next)
 (global-set-key (kbd "<S-f10>") 'bm-previous)
-(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
 (global-set-key [C-f3] 'highlight-symbol-at-point)
-(global-set-key (kbd "<f4>") 'yank)
-(global-set-key [(f5)] 'gud-cont)
 (global-set-key [(f7)] 'recompile)
 (global-set-key [C-f7] 'compile)
 (global-set-key (kbd "<f8>") 'recentf-open-files)
 (global-set-key (kbd "<f9>") 'ff-get-other-file)
-(global-set-key [(f10)] 'gud-next)
-(global-set-key [(f11)] 'gud-step)
+;; (global-set-key [(f5)] 'gud-cont)
+;; (global-set-key [(f10)] 'gud-next)
+;; (global-set-key [(f11)] 'gud-step)
 (global-set-key [(f12)] (lambda () (interactive) (kill-buffer (current-buffer))))
-;;; global-set-key [(f12)] 'delete-frame)
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'backward-kill-word)
 
 
 (define-key global-map (kbd "C-z") 'undo-tree-undo)
 (define-key global-map (kbd "C-S-z") 'undo-tree-redo)
-(define-key global-map (kbd "C-;") 'evilnc-comment-or-uncomment-lines)
-(define-key global-map (kbd "C-'") 'rg)
+(define-key global-map (kbd "C-;") 'comment-line)
 (define-key global-map (kbd "C-x C-z") 'fzf)
 (define-key global-map (kbd "C-`") 'fzf-projectile)
 
-(define-key global-map (kbd "M-RET") 'ace-jump-mode)
+(define-key global-map (kbd "M-RET") 'avy-goto-char)
+
+(global-set-key [(meta up)]  'move-line-up)
+(global-set-key [(meta down)]  'move-line-down)
 (global-set-key [(control up)]  'move-line-up)
 (global-set-key [(control down)]  'move-line-down)
+
 (global-set-key [(control l)] 'kill-whole-line)
 (global-set-key [?\e deletechar] 'kill-word)
 
@@ -456,8 +679,11 @@ When `universal-argument' is called first, cut whole buffer (but respect `narrow
 (global-set-key (kbd "<left-fringe> <mouse-4>") 'bm-previous-mouse)
 (global-set-key (kbd "<left-fringe> <mouse-1>") 'bm-toggle-mouse)
 
+(xterm-mouse-mode t)
+(global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
+(global-set-key   [mouse-5] '(lambda () (interactive) (scroll-up   1)))
+
 ;;; My Key Bindings
-(global-set-key [?\C-h] 'delete-backward-char)
 
 (let ((elapsed (float-time (time-subtract (current-time)
                                           emacs-start-time))))
@@ -480,23 +706,64 @@ When `universal-argument' is called first, cut whole buffer (but respect `narrow
  '(custom-safe-themes
    (quote
     ("c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" default)))
+ '(dashboard-center-content t)
+ '(dashboard-items (quote ((recents . 15) (projects . 5) (bookmarks . 5))) t)
+ '(dashboard-startup-banner 4 t)
+ '(dimmer-exclusion-regexp-list
+   (quote
+    (".*Minibuf.*" ".*which-key.*" ".*NeoTree.*" ".*Messages.*" ".*Async.*" ".*Warnings.*" ".*LV.*" ".*Ilist.*")) t)
+ '(dimmer-fraction 0.5)
  '(exec-path-from-shell-check-startup-files nil)
  '(exec-path-from-shell-variables (quote ("PATH" "GOPATH")))
+ '(flyspell-issue-message-flag nil t)
+ '(git-gutter:added-sign "+")
+ '(git-gutter:deleted-sign "-")
+ '(git-gutter:modified-sign "~")
+ '(global-anzu-mode t)
+ '(ispell-extra-args
+   (quote
+    ("--sug-mode=ultra" "--lang=en_US" "--run-together")) t)
+ '(ispell-program-name "aspell" t)
+ '(markdown-bold-underscore t)
+ '(markdown-enable-math t)
+ '(markdown-header-scaling t)
+ '(markdown-hide-markup nil)
+ '(markdown-hide-urls nil)
+ '(markdown-indent-function t)
+ '(markdown-italic-underscore t)
  '(package-selected-packages
    (quote
-    (company yasnippet helm-swoop miniedit diminish use-package)))
+    (deadgrep beacon highlight-symbol fill-column-indicator yaml-mode logview markdown-toc markdown-mode flyspell-correct-ivy anzu posframe company yasnippet helm-swoop miniedit diminish use-package)))
  '(recentf-auto-cleanup (quote never) t)
  '(recentf-exclude
    (quote
     ((expand-file-name package-user-dir)
      ".cache" "cache" "recentf" "COMMIT_EDITMSG\\'")) t)
- '(recentf-max-saved-items 20000000 t))
+ '(recentf-max-saved-items 20000000 t)
+ '(show-paren-mode t)
+ '(show-paren-style (quote mixed))
+ '(show-paren-when-point-in-periphery t)
+ '(show-paren-when-point-inside-paren t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(dashboard-heading ((t (:foreground "#f1fa8c" :weight bold))))
+ '(flyspell-duplicate ((t (:underline (:color "#50fa7b" :style wave)))))
+ '(flyspell-incorrect ((t (:underline (:color "#f1fa8c" :style wave)))))
+ '(font-lock-variable-name-face ((t (:foreground "violet"))))
+ '(git-gutter:added ((t (:foreground "#50fa7b" :background "#50fa7b"))))
+ '(git-gutter:deleted ((t (:foreground "#ff79c6" :background "#ff79c6"))))
+ '(git-gutter:modified ((t (:foreground "#f1fa8c" :background "#f1fa8c"))))
+ '(markdown-header-delimiter-face ((t (:foreground "mediumpurple"))))
+ '(markdown-header-face-1 ((t (:foreground "violet" :weight bold :height 1.0))))
+ '(markdown-header-face-2 ((t (:foreground "lightslateblue" :weight bold :height 1.0))))
+ '(markdown-header-face-3 ((t (:foreground "mediumpurple1" :weight bold :height 1.0))))
+ '(markdown-link-face ((t (:background "#0e1014" :foreground "#bd93f9"))))
+ '(markdown-list-face ((t (:foreground "mediumpurple"))))
+ '(markdown-pre-face ((t (:foreground "#bd98fe"))))
+ '(show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c")))))
 
 ;;----------------------------------------------------------------------------
 ;; Goanna stuff
@@ -506,4 +773,5 @@ When `universal-argument' is called first, cut whole buffer (but respect `narrow
 (load "~/.emacs.d/elisp/goanna/gpsl-mode.el")
 (load "~/.emacs.d/elisp/goanna/gxsl-mode.el")
 
-(setq initial-buffer-choice "~/.emacs.d/init.el")
+;;; (setq initial-buffer-choice "~/.emacs.d/init.el")
+(setq initial-buffer-choice 'recentf-open-files) 
